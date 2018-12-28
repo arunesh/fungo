09ae9bdb36120bb2b26c64207d5b857b26c134a3
Arunesh Mishra <aruneshm@gmail.com>
2018-12-27 21:29:11 -0800

    minor changes

--- newhw.go
+++ newhw.go
@@ -6,7 +6,26 @@ import "time"
 
 const s = "hello jupiter"
 
+
+func initSeq() func () int {
+    i := 0
+    return func () int {
+        i++
+        return i
+    }
+}
+
 func main() {
+    nextSeq := initSeq()
+    fmt.Println("next = ", nextSeq())
+    fmt.Println("next = ", nextSeq())
+    fmt.Println("next = ", nextSeq())
+
+    nextSeq2 := initSeq()
+    fmt.Println("next2 = ", nextSeq2())
+    fmt.Println("next2 = ", nextSeq2())
+    fmt.Println("next = ", nextSeq())
+
     fmt.Println("A demo of slicing.")
 
     var p = []int{2, 4, 7, 13, 16, 24}
@@ -16,8 +35,38 @@ func main() {
     fmt.Println(p)
     fmt.Println(s)
 
+    sum := 0
+    for _, v := range p {
+        sum = sum + v
+    }
+    fmt.Printf("sum = %d\n", sum)
+
     printRand()
     fmt.Println(time.Now())
+
+    newSlice := make([]string, 1, 100)
+    fmt.Printf("Size of string: %d, capacity: %d\n", len(newSlice), cap(newSlice))
+
+    someFunc := printRand
+
+    someFunc()
+
+    fmt.Println("A ")
+    fmt.Println(p)
+
+    m := make(map[string]int)
+    m["a"] = 1
+    m["b"] = 2
+
+    for k, v := range m {
+        fmt.Println("key :", k, " value:", string(v))
+        fmt.Printf("value: %d\n:", v)
+    }
+
+    for i, c := range "Arunesh Mishra" {
+        fmt.Println("i = ", i, " c = ", c)
+    }
+    fmt.Println("Function Sum =", doSum(2, 5, 6))
 }
 
 func printRand() {
@@ -40,5 +89,13 @@ func printRand() {
     }
 
     return
+}
 
+func doSum(vals ...int) int {
+    fmt.Println("Number of values:", len(vals))
+    sum := 0
+    for _, v := range vals {
+        sum += v
+    }
+    return sum
 }
